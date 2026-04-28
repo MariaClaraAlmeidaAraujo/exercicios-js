a9309d07f9bfbc623686d0a9d16327414861410b
MariaClaraAlmeidaAraujo <mcaa1@aluno.ifnmg.edu.br>
2026-04-28 19:36:42 +0000

    feat: pratica async/await e manipulação de JSON

--- exercicios/aula07/index.js
+++ exercicios/aula07/index.js
@@ -7,16 +7,23 @@
 
 // Exercício 1 — objetoParaJSON
 export function objetoParaJSON(objeto) {
+  const objetoParaJSON = JSON.stringify(objeto);
+  return objetoParaJSON
   // escreva seu código aqui
 }
 
 // Exercício 2 — jsonParaObjeto
 export function jsonParaObjeto(texto) {
+  const objeto = JSON.parse(texto);
+  return objeto
   // escreva seu código aqui
 }
 
 // Exercício 3 — clonarObjeto
 export function clonarObjeto(objeto) {
+  const original = JSON.stringify(objeto);
+  const copia = JSON.parse(original);
+  return copia
   // escreva seu código aqui
 }
 
@@ -34,10 +41,22 @@ function simularConsulta(id) {
 
 // Exercício 4 — buscarDados
 export async function buscarDados(id) {
+  const buscarDados = await simularConsulta(id);
+  return buscarDados
   // escreva seu código aqui
 }
 
 // Exercício 5 — dividirSeguro
 export async function dividirSeguro(a, b) {
+try{
+  if (b === 0){
+    throw new Error('Divisão por zero');
+
+  }
+  return a/b;
+} catch (erro){
+  console.error(erro.message);
+  throw erro;
+}
   // escreva seu código aqui
 }
